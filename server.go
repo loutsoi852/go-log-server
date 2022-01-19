e2d4787ea25549c59ae5724bc82b864707f91d1d
loo.tsoi <loo.tsoi@idnerd.com>
2022-01-19 17:27:35 +0800

    append/read from file

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "log"
     "net/http"
+	"os"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -19,6 +20,45 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 
     fmt.Fprintf(w, "Hello!")
 }
+
+func fileAppendHandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path !="/fileAppend"{
+		http.Error(w, "404 nope", http.StatusNotFound)
+		return
+	}
+
+    // If the file doesn't exist, create it, or append to the file
+    f, err := os.OpenFile("access.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        log.Fatal(err)
+    }
+    if _, err := f.Write([]byte("appended some data\n")); err != nil {
+        log.Fatal(err)
+    }
+    if err := f.Close(); err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Fprintf(w, "Done!")
+
+}
+
+func fileReadHandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path !="/fileRead"{
+		http.Error(w, "404 nope", http.StatusNotFound)
+		return
+	}
+
+ 	data, err := os.ReadFile("access.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Stdout.Write(data)
+
+    fmt.Fprintf(w, string(data))
+
+}
+
 
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
@@ -39,6 +79,8 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
     http.Handle("/", fileServer) // New code
     http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/fileAppend", fileAppendHandler)
+	http.HandleFunc("/fileRead", fileReadHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
     if err := http.ListenAndServe("127.0.0.1:8080", nil); err != nil {
