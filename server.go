1f9a4fbe0f17308b4fee28151ad35bad9a94fe91
loo.tsoi <loo.tsoi@idnerd.com>
2022-01-19 18:35:52 +0800

    basics tour

--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
     "net/http"
 	"os"
 	"io"
+	
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
---
+++ tour.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/cmplx"
+	"math/rand"
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+var clear map[string]func() //create a map for storing clear funcs
+
+func init() {
+	clear = make(map[string]func()) //Initialize it
+	clear["linux"] = func() {
+		cmd := exec.Command("clear") //Linux example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clear["windows"] = func() {
+		cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
+
+func CallClear() {
+	value, ok := clear[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
+	if ok {                          //if we defined a clear func for that platform:
+		value() //we execute it
+	} else { //unsupported platform
+		panic("Your platform is unsupported! I can't clear terminal screen :(")
+	}
+}
+
+func add(x, y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+var c, python, java bool
+var i2, j2 int = 1, 2
+
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
+func main() {
+	CallClear()
+	fmt.Println("My favorite number is", rand.Intn(100))
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Println(math.Pi)
+	fmt.Println(add(42, 13))
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+	fmt.Println(split(17))
+
+	var i int
+	fmt.Println(i, c, python, java)
+
+	var c2, python2, java2 = true, false, "no!"
+	fmt.Println(i2, j2, c2, python2, java2)
+	k := 3
+	fmt.Println(k)
+
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+
+	var i3 int
+	var f float64
+	var b2 bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", i3, f, b2, s)
+}
